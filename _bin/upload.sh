b871f581f7adc9a9d79341a1280f0c02bf7e9e7c
anthonyjclark <anthonyjclark@gmail.com>
2024-04-29 11:19:34 -0700

    Added upload script.

---
+++ _bin/upload.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+# TODO: turn into makefile with render target
+
+password="$(security find-internet-password -s wells.campus.pomona.edu -w)"
+web_host="wells.campus.pomona.edu"
+
+# Mount
+mkdir -p _mount
+mount -t smbfs "//ajcd2020:$password@$web_host/Computer%20Science/www/classes/cs181r" _mount
+
+# Upload
+cpsync _site/ _mount/
+
+# Unmount
+until diskutil unmount _mount; do echo "Trying again..."; sleep 2; done
