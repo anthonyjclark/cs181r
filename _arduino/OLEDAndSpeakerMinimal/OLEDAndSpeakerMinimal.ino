f2d0a5c14cd217082f9c9c762becf68839d55f3f
anthonyjclark <anthonyjclark@gmail.com>
2024-06-07 15:36:04 -0700

    Cleaned up oled/speaker demo.

--- _arduino/OLEDAndSpeakerMinimal/OLEDAndSpeakerMinimal.ino
+++ _arduino/OLEDAndSpeakerMinimal/OLEDAndSpeakerMinimal.ino
@@ -1,70 +1,75 @@
-// the following code changes the display screen and then plays happy birthday. 
+// This code assumes you are using the Xiao Expansion board
 
 #include <Arduino.h>
 #include <U8x8lib.h>
 #include <Wire.h>
 
-U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(SCL, SDA, U8X8_PIN_NONE);   // OLEDs without Reset of the Display
+// Display setup
+U8X8_SSD1306_128X64_NONAME_HW_I2C display(SCL, SDA, U8X8_PIN_NONE);
 
-// creating variables to sing happy birthday
-int speakerPin = A3; 
-int length = 28; 
+// Speaker setup
+
+#define SPEAKER_PIN A3
+#define SONG_LENGTH sizeof(beats)
+
 char notes[] = "GGAGcB GGAGdc GGxecBA yyecdc";
-int beats[] = { 2, 2, 8, 8, 8, 16, 1, 2, 2, 8, 8, 8, 16, 1, 2, 2, 8, 8, 8, 8, 16, 1, 2, 2, 8, 8, 8, 16 };
+int beats[] = {2, 2, 8, 8, 8, 16, 1, 2, 2, 8, 8, 8, 16, 1, 2, 2, 8, 8, 8, 8, 16, 1, 2, 2, 8, 8, 8, 16};
 int tempo = 150;
 
-void playTone(int tone, int duration) {
-  for (long i = 0; i < duration * 1000L; i += tone * 2) {
-    digitalWrite(speakerPin, HIGH);
+void playTone(int tone, int duration)
+{
+  for (long i = 0; i < duration * 1000L; i += tone * 2)
+  {
+    digitalWrite(SPEAKER_PIN, HIGH);
     delayMicroseconds(tone);
-    digitalWrite(speakerPin, LOW);
+    digitalWrite(SPEAKER_PIN, LOW);
     delayMicroseconds(tone);
   }
 }
 
-void playNote(char note, int duration) {
-  char names[] = {'C', 'D', 'E', 'F', 'G', 'A', 'B',
-                  'c', 'd', 'e', 'f', 'g', 'a', 'b',
-                  'x', 'y'
-                 };
-  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014,
-                  956,  834,  765,  593,  468,  346,  224,
-                  655 , 715
-                };
+void playNote(char note, int duration)
+{
+  char names[] = {'C', 'D', 'E', 'F', 'G', 'A', 'B', 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'x', 'y'};
+  int tones[] = {1915, 1700, 1519, 1432, 1275, 1136, 1014, 956, 834, 765, 593, 468, 346, 224, 655, 715};
   int SPEE = 5;
 
   // play the tone corresponding to the note name
-  for (int i = 0; i < 16; i++) {
-    if (names[i] == note) {
-      int newduration = duration / SPEE;
-      playTone(tones[i], newduration);
+  for (int i = 0; i < 16; i++)
+  {
+    if (names[i] == note)
+    {
+      int newDuration = duration / SPEE;
+      playTone(tones[i], newDuration);
     }
   }
 }
 
-void setup(void) {
-  u8x8.begin();
-  u8x8.setFlipMode(1);   // set number from 1 to 3, the screen word will rotary 180
-  Serial.begin(115200);
-  pinMode(speakerPin, OUTPUT);
+void setup(void)
+{
+  display.begin();
+  display.setFlipMode(1); // Display orientation
+  pinMode(SPEAKER_PIN, OUTPUT);
 }
 
-void loop(void) {
-  u8x8.setFont(u8x8_font_chroma48medium8_r);
-  u8x8.setCursor(0, 0);
-  u8x8.print("Hello!");
+void loop(void)
+{
+  display.setFont(u8x8_font_chroma48medium8_r);
+  display.setCursor(0, 0);
+  display.print("Hello!");
   delay(1000);
-  u8x8.clear();
-  u8x8.print("Happy Birthday!");
+  display.clear();
+  display.print("Happy Birthday!");
   delay(1000);
 
-  for (int i = 0; i < length; i++) {
-    if (notes[i] == ' ') {
-      delay(beats[i] * tempo); // rest
-      Serial.print("delay");
-    } else {
+  for (int i = 0; i < SONG_LENGTH; i++)
+  {
+    if (notes[i] == ' ')
+    {
+      delay(beats[i] * tempo);
+    }
+    else
+    {
       playNote(notes[i], beats[i] * tempo);
-      Serial.print("playing notes");
     }
     delay(tempo);
   }
